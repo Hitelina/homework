8f0615c254b8d343630c15addba2ce58a9342685
Oksana <ksshmyreva94@mail.ru>
2025-06-16 22:33:51 +0300

    added main function and checks

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,17 @@
+_opers = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+          ]
+
+_sort = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+         ]
+
+
+from mypy.state import state
 from datetime import datetime
 from typing import List, Dict, Union
 
@@ -12,6 +26,7 @@ def filter_by_state(data: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
     """
     return [item for item in data if item.get('state') == state]
 
+
 def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
     """
     Сортирует список словарей по дате (ключ 'date').
@@ -21,3 +36,7 @@ def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
     :return: Новый список, отсортированный по дате.
     """
     return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)
+
+if __name__ == '__main__':
+    print(filter_by_state(_opers))
+    print(sort_by_date(_sort))
