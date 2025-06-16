ab6ea71acf2680eb4e20d7942e21e80a7339b12a
Oksana <ksshmyreva94@mail.ru>
2025-06-16 22:07:28 +0300

    function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List, Dict, Union
 
 
@@ -10,3 +11,13 @@ def filter_by_state(data: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
     :return: Новый список словарей, содержащий только те, у которых 'state' соответствует указанному значению.
     """
     return [item for item in data if item.get('state') == state]
+
+def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
+    """
+    Сортирует список словарей по дате (ключ 'date').
+
+    :param data: Список словарей.
+    :param reverse: Порядок сортировки (True - убывание, False - возрастание, по умолчанию True).
+    :return: Новый список, отсортированный по дате.
+    """
+    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)
