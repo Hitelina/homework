3b3a6f6a9963d77e73e3aa1f2b7d235578043a84
Oksana <ksshmyreva94@mail.ru>
2025-06-16 22:43:41 +0300

    fixced

--- src/processing.py
+++ src/processing.py
@@ -1,22 +1,22 @@
-_opers = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-          ]
-
-_sort = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-         ]
-
-
-from mypy.state import state
 from datetime import datetime
-from typing import List, Dict, Union
+from typing import List, Dict
+
+_opers = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+_sort = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 
 
-def filter_by_state(data: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
+def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """
     Фильтрует список словарей по значению ключа 'state'.
 
@@ -24,7 +24,7 @@ def filter_by_state(data: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
     :param state: Значение ключа 'state' для фильтрации (по умолчанию 'EXECUTED').
     :return: Новый список словарей, содержащий только те, у которых 'state' соответствует указанному значению.
     """
-    return [item for item in data if item.get('state') == state]
+    return [item for item in data if item.get("state") == state]
 
 
 def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
@@ -35,8 +35,11 @@ def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
     :param reverse: Порядок сортировки (True - убывание, False - возрастание, по умолчанию True).
     :return: Новый список, отсортированный по дате.
     """
-    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)
+    return sorted(
+        data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(filter_by_state(_opers))
     print(sort_by_date(_sort))
